541689ba91a41deca2bf2b5f33219e4f1d6dcedc
Khushi Gupta <khushig1592005@gmail.com>
2025-11-14 19:17:54 +0530

    AGain pushed  the cicd

--- backend/tests/setup.js
+++ backend/tests/setup.js
@@ -1,19 +1,48 @@
-const mongoose=require('mongoose')
-const {MongoMemoryServer}=require('mongodb-memory-server')
+// const mongoose=require('mongoose')
+// const {MongoMemoryServer}=require('mongodb-memory-server')
 
-let mongoServer;
+// let const mongoose = require('mongoose');
 
-beforeAll(async()=>{
-    mongoServer=await MongoMemoryServer.create()
-    const uri=mongoServer.getUri()
-    await mongoose.connect(uri)
-})
+// beforeAll(async () => {
+//   const uri = process.env.MONGO_URI; // GitHub Actions injects this
+//   await mongoose.connect(uri);
+// });
 
-afterAll(async()=>{
-    await mongoose.disconnect()
-    await mongoServer.stop()
-})
+// afterAll(async () => {
+//   await mongoose.disconnect();
+// });
 
-beforeEach(async()=>{
-    await mongoose.connection.db.dropDatabase()
-})
+// beforeEach(async () => {
+//   await mongoose.connection.db.dropDatabase();
+// });
+// mongoServer;
+
+// beforeAll(async()=>{
+//     mongoServer=await MongoMemoryServer.create()
+//     const uri=mongoServer.getUri()
+//     await mongoose.connect(uri)
+// })
+
+// afterAll(async()=>{
+//     await mongoose.disconnect()
+//     await mongoServer.stop()
+// })
+
+// beforeEach(async()=>{
+//     await mongoose.connection.db.dropDatabase()
+// })
+
+const mongoose = require('mongoose');
+
+beforeAll(async () => {
+  const uri = process.env.MONGO_URI; // GitHub Actions injects this
+  await mongoose.connect(uri);
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+});
+
+beforeEach(async () => {
+  await mongoose.connection.db.dropDatabase();
+});
